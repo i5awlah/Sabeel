9af60d2c2b7c8a307b0e4305a75bd4db098a687f
Khawlah <khawlah_alrashed@hotmail.com>
2023-02-03 12:43:10 +0300

    Initial Commit

---
+++ Sabeel/SabeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabeelApp.swift
+//  Sabeel
+//
+//  Created by Khawlah on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SabeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
