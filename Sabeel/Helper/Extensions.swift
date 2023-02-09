739b5385b69bf1e4a91615d16f05afd78a3e035d
Khawlah <khawlah_alrashed@hotmail.com>
2023-02-09 10:50:50 +0300

    add colors

--- Sabeel/Helper/Extensions.swift
+++ Sabeel/Helper/Extensions.swift
@@ -5,4 +5,13 @@
 //  Created by Khawlah on 06/02/2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let customRed = Color("customRed")
+    static let darkBlue = Color("darkBlue")
+    static let darkGray = Color("darkGray")
+    static let lightBlue = Color("lightBlue")
+    static let lightGray = Color("lightGray")
+}
+
