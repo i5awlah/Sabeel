d5d32da816cbc91c40331800a4576ddf3ea68c3c
Einas <et86et@hotmail.com>
2023-02-13 17:01:52 +0300

    edited PecsModel and PecsViewModel by Einas

--- Sabeel/Model/PecsModel.swift
+++ Sabeel/Model/PecsModel.swift
@@ -2,41 +2,46 @@
 //  PecsModel.swift
 //  Sabeel
 //
-//  Created by Khawlah on 06/02/2023.
+//  Created by Einas Alturki on 12/02/2023.
 //
 
 import CloudKit
 
 struct PecsModel: Identifiable {
     let id: String
+    let category: String
     let imageURL: URL?
     let audioURL: URL?
     let name: String
-    var startTime: Date
-    var endTime: Date
+//    var startTime: Date
+//    var endTime: Date
     let associatedRecord: CKRecord
     
     // For easy access to name fields
     fileprivate static let keys = (
+        category: "category",
         imageURL: "imageURL",
         audioURL: "audioURL",
-        name : "name",
-        startTime : "startTime",
-        endTime : "endTime"
+        name: "name"
+//        startTime : "startTime",
+//        endTime : "endTime"
     )
 }
 
 extension PecsModel {
     init?(record: CKRecord) {
         guard let name = record.value(forKey: PecsModel.keys.name) as? String
-                , let startTime = record.value(forKey: PecsModel.keys.startTime) as? Date
-                , let endTime = record.value(forKey: PecsModel.keys.endTime) as? Date else { return nil }
+                ,let category = record.value(forKey: PecsModel.keys.category) as? String
+//                , let startTime = record.value(forKey: PecsModel.keys.startTime) as? Date
+//                , let endTime = record.value(forKey: PecsModel.keys.endTime) as? Date
+        else { return nil }
         
         self.id = record.recordID.recordName
         self.associatedRecord = record
         self.name = name
-        self.startTime = startTime
-        self.endTime = endTime
+        self.category = category
+//        self.startTime = startTime
+//        self.endTime = endTime
         
         let imageAsset = record.value(forKey: PecsModel.keys.imageURL) as? CKAsset
         self.imageURL = imageAsset?.fileURL
@@ -49,8 +54,9 @@ extension PecsModel {
         
         var dictonary : [String: Any] = [
             PecsModel.keys.name : name,
-            PecsModel.keys.startTime : startTime,
-            PecsModel.keys.endTime : endTime
+            PecsModel.keys.category : category
+//            ,PecsModel.keys.startTime : startTime,
+//            PecsModel.keys.endTime : endTime
         ]
         
         if let imageURL {
--- Sabeel/View Model/PecsViewModel.swift
+++ Sabeel/View Model/PecsViewModel.swift
@@ -1,8 +1,40 @@
-//
-//  PecsViewModel.swift
-//  Sabeel
-//
-//  Created by Khawlah on 06/02/2023.
-//
+// Created by Einas on 12/02/2023.
 
 import Foundation
+import CloudKit
+
+class PECSViewModel: ObservableObject {
+    
+    private var container: CKContainer
+    @Published var iCloudAvailable: Bool = false
+    @Published var currentUser: UserModel?
+    
+    init() {
+        self.container = CKContainer.default()
+        
+    }
+    
+    //Fetch all PECS from Home_Content for one child
+    //Home_Content fetches from PECS and Custom_PECS
+    func fetchPECS(){
+        let predicate = NSPredicate(value: true)
+//        let query = CKQuery(recordType: "Home_Content", predicate: predicate)
+//        container.privateCloudDatabase.fetch(withQuery: query) { result in
+//            switch(result) {
+    }
+    
+    // 1- Delete PECS form Home_Content the record that refers to it
+    // 2- Check the PECS TYPE if it is PECS or Custom PECS
+    //      2.1 if it's Custom PECS delete it from Home_Content and Custom_PECS
+    //      2-2 if it's PECS delete the record only from Home_Content
+    //NEVER DELETE FROM PECS TABLE
+    func deletePECS(){
+        
+    }
+    
+    func hidePECS(){
+        
+    }
+    
+    
+}
