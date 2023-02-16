7729962d5792b1bd28869c8ac3fdb5d4df8958d8
Ashwaq3 <eng.ashwaq.h.a@hotmail.com>
2023-02-16 11:25:50 +0300

    settings and schedule ui

--- Sabeel/View/Settings/SchedulePecsView.swift
+++ Sabeel/View/Settings/SchedulePecsView.swift
@@ -2,19 +2,156 @@
 //  SchedulePecsView.swift
 //  Sabeel
 //
-//  Created by Khawlah on 08/02/2023.
+//  Created by Ashwaq on 16/02/2023.
 //
 
 import SwiftUI
 
 struct SchedulePecsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    static var uniqueKey: String {
+        UUID().uuidString
     }
-}
+    static let options: [DropdownOption] = [
+        DropdownOption(key: uniqueKey, value: "Food"),
+        DropdownOption(key: uniqueKey, value: "Utensils"),
+        DropdownOption(key: uniqueKey, value: "Clothes"),
+        DropdownOption(key: uniqueKey, value: "Bathroom"),
+        DropdownOption(key: uniqueKey, value: "Activities"),
+        DropdownOption(key: uniqueKey, value: "Feelings"),
+        DropdownOption(key: uniqueKey, value: "Family"),
+        DropdownOption(key: uniqueKey, value: "people"),
+        DropdownOption(key: uniqueKey, value: "places"),
+        DropdownOption(key: uniqueKey, value: "Tools")
+    ]
+    var body: some View {
+        HStack{
+            Text("Category")
+            Group {
+                DropdownSelector(
+                    placeholder: "Select Option",
+                    options: SchedulePecsView.options,
+                    onOptionSelected: { option in
+                        print(option)
+                    })
+                .padding(.horizontal)
+            }}.background()
+    }
+   }
+
 
 struct SchedulePecsView_Previews: PreviewProvider {
     static var previews: some View {
         SchedulePecsView()
     }
 }
+
+
+
+
+
+struct DropdownSelector: View {
+    @State private var shouldShowDropdown = false
+       @State private var selectedOption: DropdownOption? = nil
+       var placeholder: String
+       var options: [DropdownOption]
+       var onOptionSelected: ((_ option: DropdownOption) -> Void)?
+       private let buttonHeight: CGFloat = 45
+
+       var body: some View {
+           Button(action: {
+               self.shouldShowDropdown.toggle()
+           }) {
+               HStack {
+                   Text(selectedOption == nil ? placeholder : selectedOption!.value)
+                       .font(.system(size: 14))
+                       .foregroundColor(selectedOption == nil ? Color.gray: Color.black)
+
+                   Spacer()
+
+                   Image(systemName: self.shouldShowDropdown ? "arrowtriangle.up.fill" : "arrowtriangle.down.fill")
+                       .resizable()
+                       .frame(width: 9, height: 5)
+                       .font(Font.system(size: 9, weight: .medium))
+                       .foregroundColor(Color.black)
+               }
+           }
+           .padding(.horizontal)
+           .cornerRadius(5)
+           .frame(width: .infinity, height: self.buttonHeight)
+           .overlay(
+               RoundedRectangle(cornerRadius: 5)
+                   .stroke(Color.gray, lineWidth: 1)
+           )
+           .overlay(
+               VStack {
+                   if self.shouldShowDropdown {
+                       Spacer(minLength: buttonHeight + 10)
+                       Dropdown(options: self.options, onOptionSelected: { option in
+                           shouldShowDropdown = false
+                           selectedOption = option
+                           self.onOptionSelected?(option)
+                       })
+                   }
+               }, alignment: .topLeading
+           )
+           .background(
+               RoundedRectangle(cornerRadius: 5).fill(Color.white)
+           )
+       }
+   }
+
+struct DropdownOption: Hashable {
+    let key: String
+    let value: String
+
+    public static func == (lhs: DropdownOption, rhs: DropdownOption) -> Bool {
+        return lhs.key == rhs.key
+    }
+}
+
+
+struct Dropdown: View {
+    var options: [DropdownOption]
+    var onOptionSelected: ((_ option: DropdownOption) -> Void)?
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(self.options, id: \.self) { option in
+                    DropdownRow(option: option, onOptionSelected: self.onOptionSelected)
+                }
+            }
+        }
+        .frame(minHeight: CGFloat(options.count) * 30, maxHeight: 250)
+        .padding(.vertical, 5)
+        .background(Color.white)
+        .cornerRadius(5)
+        .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+    }
+}
+
+struct DropdownRow: View {
+    var option: DropdownOption
+    var onOptionSelected: ((_ option: DropdownOption) -> Void)?
+
+    var body: some View {
+        Button(action: {
+            if let onOptionSelected = self.onOptionSelected {
+                onOptionSelected(self.option)
+            }
+        }) {
+            HStack {
+                Text(self.option.value)
+                    .font(.system(size: 14))
+                    .foregroundColor(Color.black)
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 5)
+    }
+}
+
--- Sabeel/View/Settings/SettingsView.swift
+++ Sabeel/View/Settings/SettingsView.swift
@@ -2,14 +2,54 @@
 //  SettingsView.swift
 //  Sabeel
 //
-//  Created by Khawlah on 06/02/2023.
+//  Created by Ashwaq on 14/02/2023.
 //
 
 import SwiftUI
 
 struct SettingsView: View {
+    var settingsData = [
+        SettingTile(title: "Link your Autistic ", icon: "link.icloud", navigationDestination: ChildQRView()),
+        SettingTile(title:"Scheduled PECS", icon: "list.bullet.clipboard", navigationDestination: SchedulePecsView()),
+ 
+      ];
+    @State private var number: Int = 2
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        NavigationStack {
+            List {
+                ForEach(settingsData) {data in
+                    
+                    NavigationLink(destination: SchedulePecsView()) {
+                        HStack{
+                            Image(systemName:data.icon).frame(width: 35, height: 30).background(Color("buttonBlue")).foregroundColor(.white).cornerRadius(5)
+                            Text(data.title)
+       
+                        }.padding(EdgeInsets(.init(top: 8, leading: 0, bottom: 8, trailing: 0)))
+                    } .buttonStyle(.plain)
+                    
+          
+                   
+               
+                }
+                
+                HStack{
+                    Image(systemName:"photo.on.rectangle").frame(width: 35, height: 30).background(Color("buttonBlue")).foregroundColor(.white).cornerRadius(5)
+                    Text("Number of PECS per row:")
+                    Spacer()
+              
+
+                            Picker("", selection: $number) {
+                                ForEach(1...5, id: \.self) { number in
+                                    Text("\(number)")
+                                }
+                            }
+                                .labelsHidden()
+
+                }.padding(EdgeInsets(.init(top: 8, leading: 0, bottom: 8, trailing: 0)))
+            }.navigationTitle("Settings")
+            
+        }
     }
 }
 
@@ -18,3 +58,22 @@ struct SettingsView_Previews: PreviewProvider {
         SettingsView()
     }
 }
+
+struct SettingTile : Identifiable{
+    let id = UUID()
+    
+    let title: String
+    let icon :String
+    let navigationDestination :any View
+    
+    init(title:String, icon: String,navigationDestination :any View) {
+        self.title = title
+        self.icon = icon
+        self.navigationDestination = navigationDestination
+        
+        
+        
+   
+    }
+}
+
