21d008f53274c1d79107bb971f60f97948287404
Khawlah <khawlah_alrashed@hotmail.com>
2023-02-16 12:31:19 +0300

    edit notification view

--- Sabeel/View/Notifications/NotificationsView.swift
+++ Sabeel/View/Notifications/NotificationsView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct NotificationsView: View {
+    
+    @EnvironmentObject var cloudViewModel: CloudViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(cloudViewModel.childRequests
+                .sorted(by: { $0.associatedRecord.creationDate ?? .now > $1.associatedRecord.creationDate ?? .now }), id: \.id) { childRequest in
+                HStack {
+                    if let date = childRequest.associatedRecord.creationDate {
+                        Text("\(date.polite)")
+                    }
+                    
+                    Text("\(childRequest.pecs.name)")
+                }
+            }
+        }
     }
 }
 
 struct NotificationsView_Previews: PreviewProvider {
     static var previews: some View {
         NotificationsView()
+            .environmentObject(CloudViewModel())
     }
 }
