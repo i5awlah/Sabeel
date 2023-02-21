9246d0d9d323d0ba963e964b768aedc62314c547
Khawlah <khawlah_alrashed@hotmail.com>
2023-02-22 01:18:36 +0300

    add view for adding child

--- Sabeel/View/Settings/SettingsView.swift
+++ Sabeel/View/Settings/SettingsView.swift
@@ -18,14 +18,6 @@ struct SettingsView: View {
     @AppStorage("number0fColumns") var gridRows = 2
     
     @EnvironmentObject var cloudViewModel: CloudViewModel
-    @StateObject private var vm = ScanViewModel()
-    
-    @State private var isPresentedScan = false
-    
-    @State private var showScannerStatusAlert = false
-    @State private var scannerStatusAlertTitle = ""
-    
-    @State private var showAlreadyLinkedView = false
     
     var body: some View {
     
@@ -38,11 +30,12 @@ struct SettingsView: View {
                         SettingsCellView(data: settingsDataLinkParent)
                     }
                 } else {
-                    SettingsCellView(data: settingsDataLinkChild)
-                        .onTapGesture {
-                            cloudViewModel.childParentModel == nil ?
-                            scanButtonPressed() : showAlreadyLinkedView.toggle()
-                        }
+                    NavigationLink {
+                        cloudViewModel.childParentModel == nil ?
+                        AnyView(AddChildView()) : AnyView(AlreadyLinkedView())
+                    } label: {
+                        SettingsCellView(data: settingsDataLinkChild)
+                    }
                 }
 
                 // 2- Schedule
@@ -76,22 +69,8 @@ struct SettingsView: View {
                 }.padding(EdgeInsets(.init(top: 8, leading: 0, bottom: 8, trailing: 0)))
             }
             .navigationTitle("Settings")
-            .navigationDestination(isPresented: $isPresentedScan, destination: {
-                ScanQRView(
-                    isPresentedScan: $isPresentedScan,
-                    addChild: addChild
-                )
-            })
-            .navigationDestination(isPresented: $showAlreadyLinkedView, destination: {
-                AlreadyLinkedView()
-            })
-            .alert(scannerStatusAlertTitle.localized, isPresented: $showScannerStatusAlert) {
-                TextField("Enter your child id", text: $vm.qr)
-                Button("Add", action: addChild)
-                Button("Cancel", role: .cancel) { }
-            }
+
         }
-        .environmentObject(vm)
     }
 }
 
@@ -110,44 +89,5 @@ struct SettingTile : Identifiable{
     init(title:String, icon: String) {
         self.title = title
         self.icon = icon
-    }
-}
-
-extension SettingsView {
-    func addChild() {
-        guard let currentUser = cloudViewModel.currentUser else { return }
-        
-        let childID = vm.qr
-        vm.qr = ""
-        // get child record
-        cloudViewModel.fetchChild(childID: childID) { child in
-            cloudViewModel.addChildToParent(child: child, parent: currentUser)
-        }
-    }
-    
-    func showAlert(_ title: String) {
-        scannerStatusAlertTitle = title
-        showScannerStatusAlert.toggle()
-    }
-    
-    func scanButtonPressed() {
-        Task {
-            await vm.requestDataScannerAccessStatus()
-            
-            switch vm.dataScannerAccessStatus {
-            case .scannerAvailable:
-                withAnimation(.spring()) {
-                    isPresentedScan.toggle()
-                }
-            case .cameraNotAvailable:
-                showAlert("Your device doesn't have a camera")
-            case .scannerNotAvailable:
-                showAlert("Your device doesn't have support for scanning barcode with this app")
-            case .cameraAccessNotGranted:
-                showAlert("Please provide access to the camera in settings")
-            case .notDetermined:
-                showAlert("Requesting camera access")
-            }
-        }
     }
 }
