81e90d374ccbb1518fa34ee5cc4189dfef132e15
Ashwaq3 <eng.ashwaq.h.a@hotmail.com>
2023-02-22 20:08:46 +0300

    chore: previous scheduled pecs category

--- Sabeel/View/Settings/SchedulePecsView.swift
+++ Sabeel/View/Settings/SchedulePecsView.swift
@@ -11,7 +11,11 @@ struct SchedulePecsView: View {
     static var uniqueKey: String {
         UUID().uuidString
     }
+
+
+    @AppStorage("schedule1") var scheduleCategories: Data = Data()
    
+
     static let options: [DropdownOption] = [
         DropdownOption(key: uniqueKey, value: NSLocalizedString("Food", comment: "")),
         DropdownOption(key: uniqueKey, value:  NSLocalizedString("Drink", comment: "")),
@@ -27,7 +31,15 @@ struct SchedulePecsView: View {
     ]
     @State private var fromTime = Date.now
     @State private var toTime = Date.now
+    @State private var optioncategory = ""
+
+    @State private var output1: [sech1] = []
+    @State private var showPreviousSec = false
+    var userDefaults = UserDefaults.standard
+
     var body: some View {
+   
+
         ZStack{
             Color.lightGray.ignoresSafeArea()
             VStack{
@@ -43,6 +55,8 @@ struct SchedulePecsView: View {
                               ,
                             options: SchedulePecsView.options,
                             onOptionSelected: { option in
+
+                                optioncategory = option.value
                                 print(option)
                                 
                             }).padding()
@@ -67,22 +81,92 @@ struct SchedulePecsView: View {
                   
                 
                 Button {
+                                        let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "HH:mm"
+                
+                    let record2 = sech1(category: optioncategory, fromTime: dateFormatter.string(from: fromTime), toTime: dateFormatter.string(from:toTime))
+                    
+                
+                    if let valueIndex = output1.firstIndex(where: {$0.category == record2.category}){
+                        output1[valueIndex] = record2
+                    }else{
+                        output1.append(record2)
+                    }
+                
+                    
+                
+                    let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: output1)
+                    userDefaults.set(encodedData, forKey: "sec")
+
+                    
+                    
                 } label: {
+                    
                     Text("Schedule It").padding(.vertical, 12).font(Font.customFont(size: 20))
                 }
                 .frame(maxWidth: .infinity) .foregroundColor(.white)
                 .background(Color.buttonBlue)      .cornerRadius(10).padding(.all,20)
                 
-         
-          
+                
             
-            } .frame(maxHeight: .infinity, alignment: .top)  .navigationTitle("Schedule PECS")
+                
+                
+        
+                Divider().padding()
+                
+                Button{
+                    if(output1.isEmpty){
+                        let decoded  = userDefaults.data(forKey: "sec")
+                        if(decoded == nil){
+                            let decodedTeams = NSKeyedUnarchiver.unarchiveObject(with: decoded!) as! [sech1]
+                            print("OUTPUT2 \(decodedTeams[0].category)")
+                            
+                            output1 = decodedTeams}
+                    }
+                    
+                    showPreviousSec.toggle()
+                }label: {
+                    HStack{
+                        Text("Scheduled Previously")
+                            .font(.customFont(size: 18)).bold().frame( alignment: .leading).padding(.horizontal,20).foregroundColor(Color.darkBlue)
+                        
+                        Image(systemName: showPreviousSec ? "arrowtriangle.up.fill" : "arrowtriangle.down.fill")
+                            .resizable()
+                            .frame(width: 9, height: 5)
+                            .font(Font.system(size: 9, weight: .medium))
+                            .foregroundColor(Color.darkBlue)
+                    }.frame(maxWidth: .infinity, alignment: .leading)
+                }
+                if(showPreviousSec && !output1.isEmpty){
+                    ForEach(0..<output1.count, id: \.self){ index in
+                        
+                        ScedhuleRowRowView(record: output1[index], index: index, function:{
+                            output1.remove(at: index)
+                            
+                            let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: output1)
+                            userDefaults.set(encodedData, forKey: "sec")
+                            
+                        })
+                        
+                        
+                    }}
+             
+                
+         
+
+            
+            } .frame(maxHeight: .infinity, alignment: .topLeading)  .navigationTitle("Schedule PECS")
             
             
        
            
         }.toolbar(.hidden,for: .tabBar)
-    }}
+    }
+    
+
+    
+    
+}
 
 
 struct SchedulePecsView_Previews: PreviewProvider {
@@ -158,6 +242,7 @@ struct Dropdown: View {
 
     var body: some View {
         ScrollView {
+            
             VStack(alignment: .leading, spacing: 0) {
                 ForEach(self.options, id: \.self) { option in
                     DropdownRow(option: option, onOptionSelected: self.onOptionSelected).font(Font.customFont(size: 14))
@@ -196,4 +281,75 @@ struct DropdownRow: View {
         .padding(.vertical, 5)
     }
 }
+
+
+
+
+
+class sech1: NSObject, NSCoding {
+    var category: String
+    var fromTime: String
+    var toTime: String
+
+
+    init(category: String, fromTime: String, toTime: String) {
+        self.category = category
+        self.fromTime = fromTime
+        self.toTime = toTime
+
+    }
+
+    required convenience init(coder aDecoder: NSCoder) {
+        let category = aDecoder.decodeObject(forKey: "category") as! String
+        let fromTime = aDecoder.decodeObject(forKey: "fromTime") as! String
+        let toTime = aDecoder.decodeObject(forKey: "toTime") as! String
+        self.init(category: category, fromTime:fromTime, toTime: toTime)
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(category, forKey: "category")
+        aCoder.encode(fromTime, forKey: "fromTime")
+        aCoder.encode(toTime, forKey: "toTime")
+    }
+}
+
+struct ScedhuleRowRowView : View {
+
+    private var record : sech1
+    private var index : Int
+    var function: () -> Void
+   
+
+
+    init(record : sech1, index:Int, function: @escaping () -> Void){
+        self.record = record
+        self.index = index
+        self.function = function
+    }
+    
+    var body: some View {
+        HStack{
+            Text("\(index+1)").frame(width: 35, height: 35).background(Color.darkGray).foregroundColor(.white).cornerRadius(5)
+            Spacer().frame(width: 16)
+            VStack(alignment: .leading, spacing: 2) {
+            
+                Text("\(record.category)")
+                    .font(.customFont(size: 18)).frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("From Time: \(record.fromTime) To Time: \(record.toTime)")
+                    .font(.customFont(size: 12)).frame(maxWidth: .infinity, alignment: .leading).foregroundColor(Color.darkGray)
+                
+            }
+            
+            Button{
+                function()
+            }label: {
+                Image(systemName:"trash").frame(width: 35, height: 30).foregroundColor(Color.customRed)
+            }
+        }
+      .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity,maxHeight: 50)
+    }
+}
+
 
